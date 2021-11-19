20f525dbbf4e4f047989bc74f3b4b834e7dd463e
mlianto <93750583+mlianto@users.noreply.github.com>
2021-11-19 14:53:03 +0800

    commit on master

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const { add } = require("ramda");
 
 const app = express();
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.urlencoded(app, (extended = true)));
 
 const accountData = fs.readFileSync("src/json/accounts.json", "utf8");
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync("src/json/users.json", "utf8");
 const users = JSON.parse(userData);
+
+app.get("/transfer", (req, res) => {
+  res.render("transfer");
+});
 
 app.get("/savings", (req, res) => {
   res.render("account", {
@@ -42,6 +48,32 @@ app.get("/", (req, res) => {
   res.render("index", {
     title: "Account Summary",
     accounts: accounts,
+  });
+});
+
+app.get("/payment", (req, res) => {
+  account: accounts.credit;
+});
+
+app.post("/payment", (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available =
+    parseInt(accounts.credit.available) + parseInt(req.body.amount);
+  accountsJSON = JSON.stringify(accountsJSON);
+  res.render("payment", {
+    message: "Payment Successful",
+    account: accounts.credit,
+  });
+});
+
+app.post("/transfer", (req, res) => {
+  accounts["savings"].from -= accounts["savings"].amount;
+  accounts["savings"].to += parseInt(accounts["savings"].amount);
+
+  accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync("json/accounts.json", accountsJSON);
+  res.render("/transfer", {
+    message: "Transfer Completed",
   });
 });
 
