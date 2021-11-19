a72da60cf159581450c584237f441b36263bf1ee
mlianto <93750583+mlianto@users.noreply.github.com>
2021-11-19 14:26:32 +0800

    commit on master

--- src/app.js
+++ src/app.js
@@ -8,9 +8,40 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "./public")));
 
+const accountData = fs.readFileSync("src/json/accounts.json", "utf8");
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync("src/json/users.json", "utf8");
+const users = JSON.parse(userData);
+
+app.get("/savings", (req, res) => {
+  res.render("account", {
+    account: accounts.savings,
+  });
+});
+
+app.get("/checking", (req, res) => {
+  res.render("account", {
+    account: accounts.checking,
+  });
+});
+
+app.get("/credit", (req, res) => {
+  res.render("account", {
+    account: accounts.credit,
+  });
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile", {
+    user: users[0],
+  });
+});
+
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Index",
+    title: "Account Summary",
+    accounts: accounts,
   });
 });
 
