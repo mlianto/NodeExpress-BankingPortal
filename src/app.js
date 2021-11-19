66150c9e57f3126b4d11e4c377c384a6af79ee52
mlianto <93750583+mlianto@users.noreply.github.com>
2021-11-19 15:33:09 +0800

    commit on master

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 
-const { accounts, users, writeJSON } = require("./data.js");
-
 const app = express();
+
+const { accounts, users, writeJSON } = require("./data");
+
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 
